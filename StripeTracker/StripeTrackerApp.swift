cb8ac3cc726478855a1328c16484384496c81fa6
mustafaalhayali <mustafamalhayali@gmail.com>
2025-07-29 14:08:35 -0400

    Initial Commit

---
+++ StripeTracker/StripeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripeTrackerApp.swift
+//  StripeTracker
+//
+//  Created by Mustafa on 2025-07-29.
+//
+
+import SwiftUI
+
+@main
+struct StripeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
